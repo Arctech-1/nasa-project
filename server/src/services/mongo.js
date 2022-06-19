6943dae40dac6005a184195ab1ebb972156f5f92
Reuben <63545265+Arctech-1@users.noreply.github.com>
2022-06-19 10:43:44 +0100

    add mongo db for github actions

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const MONGO_URL = "mongodb+srv://nasa-api:lnAD12W3sv2P0UOq@nasacluster.5pt24vi.mongodb.net/?retryWrites=true&w=majority";
 
+require("dotenv").config();
 /**
  * Listen to success and error events emitted by mongoose
  */
@@ -8,7 +8,7 @@ mongoose.connection.once("open", () => console.log("MongoDB connection ready"));
 mongoose.connection.on("error", (err) => console.error(err));
 
 async function mongoConnect() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(process.env.MONGO_URL);
 }
 
 async function mongoDisconnect() {
