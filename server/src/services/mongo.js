0bb45ee60058369853c4d18cb95d745486b274a1
Reuben <63545265+Arctech-1@users.noreply.github.com>
2022-06-19 13:31:51 +0100

    removed console.error

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -5,7 +5,7 @@ require("dotenv").config();
  * Listen to success and error events emitted by mongoose
  */
 mongoose.connection.once("open", () => console.log("MongoDB connection ready"));
-mongoose.connection.on("error", (err) => console.error("error occured"));
+mongoose.connection.on("error", (err) => console.log("error occured"));
 
 async function mongoConnect() {
   await mongoose.connect(process.env.MONGO_URL);
